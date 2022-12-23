679b9ec80a0a9dec71f032ed9728394847dba8d9
Caleb <orakporcaleb@gmail.com>
2022-12-23 01:39:09 +0100

    compressed images, edited logo, service and experience pages

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -3,17 +3,17 @@ import './experience.css'
 import CV from '../../assets/Resume.pdf'
 import CT from '../../assets/certification_logo2.jpg'
 import EDU from '../../assets/education_logo2.png'
-import {TbArrowBigRightLine} from 'react-icons/tb'
+import {TbArrowBigUpLine} from 'react-icons/tb'
 
 const Experience = () => {
   return (
     <section id="experience">
         <h1>Experience</h1>
-        <span className="flip__indicator"><p>flip</p><TbArrowBigRightLine /></span>
+        <span className="flip__indicator"><TbArrowBigUpLine /></span>
       <div className="experience__wrapper">
         <div className="flip__card">
           <div className="qualifications">
-            <div className="quali__card__back">
+            <div className="quali__card back">
               <div className="logo_div">
                 <img src={EDU} alt="Education" />
                 <h2>Education</h2>
@@ -21,13 +21,13 @@ const Experience = () => {
               <p>Bachelor of Engineering - Electronic Engineering<br />University of Nigeria Nsukka (UNN)<br />
                 <span>2014-2019</span></p>
             </div>
-            <div className="quali__card__front">
+            <div className="quali__card front">
               <div className="logo_div">
                 <img src={CT} alt="Certification" />
                 <h2>Certifications</h2>
               </div>
               <p> Responsive Web development - Udemy<br />
-              <span>Febuary 27, 2020.</span></p><br />
+              <span>Febuary 27, 2020.</span></p>
               <p>Javascript Algorithm and Data Structure - Freecodecamp<br />
               <span>July 13, 2022.</span></p>
             </div>
