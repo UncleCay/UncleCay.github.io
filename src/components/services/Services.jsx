8b70c72fb98a08e54cdb30139e5a24375060f845
Caleb <orakporcaleb@gmail.com>
2022-12-24 19:47:04 +0100

    added a skill in Services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './services.css'
 import webdev from '../../assets/webdev__icon.jpg'
 import ui_ux from '../../assets/ui_ux__icon.jfif'
+import image_editor from '../../assets/image_editor__icon.png'
 
 const Services = () => {
   return (
@@ -9,7 +10,7 @@ const Services = () => {
       <h1>Services</h1>
       <div className='serviceCards'>
         <div className="serviceCard">
-          <img src={webdev} alt="Caleb Orakpor - webdev_logo"/>
+          <img src={webdev} alt="Caleb Orakpor - webdev_icon"/>
           <h2>WEB DEVELOPMENT</h2>
           <p> I design and develop 
             any form of Reactjs or Javascrip project;<br />
@@ -18,11 +19,18 @@ const Services = () => {
           </p>
         </div>
         <div className="serviceCard">
-          <img src={ui_ux} alt="Caleb Orakpor - webdev_logo"/>
+          <img src={ui_ux} alt="Caleb Orakpor - ui-ux_icon"/>
           <h2>UI/UX</h2>
           <p> I design from scratch and improve 
             user-interface and user-experience of already existing website and web application
             using Figma and Paint graphics editor.
+          </p>
+        </div>
+        <div className="serviceCard">
+          <img src={image_editor} alt="Caleb Orakpor - Image_editor_icon"/>
+          <h2>IMAGE EDITING</h2>
+          <p> Using tools like Paint graphics editor and Adobe Photoshop,
+            I can modify any kind of image file to your specifications.
           </p>
         </div>
       </div>
