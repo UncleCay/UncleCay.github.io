4339a15bb7220d9dc90fa83112a0c9b82543f188
Caleb <orakporcaleb@gmail.com>
2023-02-08 11:08:56 +0100

    edited about page and service page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -40,12 +40,11 @@ const About = () => {
            <span className='about__scroll'>My name is Caleb. I'm a front-end React developer.</span>
           </h2>
           <p>
-          <span className="about__scroll">I started with Paint graphic editor, but the moment I executed “hello world” on the development console, I knew right away that this was my passion and since then, I have been writing more than just “hello world”. </span>
-          <span className="about__scroll">I design and develop responsive websites using React. </span>
-          <span className="about__scroll">When working on a project, I pay attention to the smallest details addressing problems with patience. </span>
-          <span className="about__scroll">Web design and development help exercise my creative thinking and satisfy my desire for constant self-growth. </span> 
-          <span className="about__scroll">When I design, I put my priority on responsiveness, scalability, simplicity, and user-friendliness. </span>
-          <span className="about__scroll">A concise, reusable, intelligible, and maintainable code is another goal of mine. I strive for constant self-growth.</span>
+          <span className="about__scroll">I started with Paint graphic editor, but after executing &quot;hello world&quot;, my first code on the development console, I knew right away that this is where I belong, and ever since then, I have been writing more than just &quot;hello world&quot;. </span>
+          <span className="about__scroll">I design and develop responsive websites. </span>
+          <span className="about__scroll">When working on a project, my priorities are responsiveness, scalability, simplicity, and user-friendliness. </span>
+          <span className="about__scroll">A concise, reusable, and maintainable code are also my aim. </span>
+          <span className="about__scroll">Through web design and development, I exercise my creative thinking and satisfy my curiosity and desire for constant self-growth.</span> 
           </p>
         </div>
       </div>
